8ffa66050475df19c89e51ee9f2cb4789032f398
saprykins <palatatot@gmail.com>
2021-07-21 09:20:38 +0300

    tsa multi-subject sends to file

--- tsa.py
+++ tsa.py
@@ -3,7 +3,11 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = 'tsa_news'
-    start_urls = ['https://www.tsa-algerie.com/economie/']
+    start_urls = [
+        'https://www.tsa-algerie.com/economie/',
+        'https://www.tsa-algerie.com/politique/',
+        'https://www.tsa-algerie.com/societe/'
+    ]
 
     def parse(self, response):
         for quote in response.css('.title-middle a'):
