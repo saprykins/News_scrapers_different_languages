9cf3dd57d98e00340a42237b6ef13c0c0e5ecf06
saprykins <palatatot@gmail.com>
2021-07-22 13:36:18 +0300

    added link scraping

--- tsa.py
+++ tsa.py
@@ -12,5 +12,23 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         for quote in response.css('.title-middle a'):
             yield {
-                'header': quote.css('a ::text').get()
+                'header': quote.css('::text').get(),
+                #'link': quote.css('::text').get()
+                'link': quote.css('a').attrib['href']
+                #'header': quote.css('a ::text').get() works well
+                #'header': quote.css('a ::href').get() doesn't work
+                #'link': quote.css('a href').get() empty list
             }
+"""
+doesn't work though due to a wrong selector
+quote = response.css("h1.article-preview__title title-middle transition")[0]
+text = quote.css("a::text").get()
+link = quote.css("a").attrib['href'].get()
+"""
+
+"""
+let's keep it
+quote = response.css('.title-middle a')[0] # works
+text = quote.css("::text").get() # works
+link = quote.css('a').attrib['href'] # works
+"""
