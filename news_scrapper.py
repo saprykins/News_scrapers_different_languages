d7f6f61cad09721d778f839d5ebaf811c5397a78
saprykins <palatatot@gmail.com>
2021-08-03 20:16:43 +0300

    sorts out politics and economics from tsa

--- news_scrapper.py
+++ news_scrapper.py
@@ -9,10 +9,13 @@ from datetime import datetime
 
 
 # sorts and writes down several objects to file
-def write_to_file(ds):
+def write_to_file(ds_pol, ds_eco):
+    # put several dataframes together
+    frames = [ds_pol, ds_eco]
+    ds = pd.concat(frames)
+
     ds = ds.sort_values(by="Date", ascending=False)
     ds.to_csv('news.csv', index=False, encoding='utf-8')
-
 
 def translate():
     pass
@@ -22,8 +25,7 @@ def aps_scraper():
     pass
 
 
-def tsa_scraper():
-    URL = "https://www.tsa-algerie.com/politique/"
+def tsa_scraper(URL, subj):
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
     results = soup.find_all('h1', class_="title-middle")
@@ -54,12 +56,17 @@ def tsa_scraper():
         dates.append(datetime.strptime(date_code_verb, '%Y-%m-%dT%H:%M:%S'))
 
     # saves to data-frame
-    # df = pd.DataFrame({'Date':dates, 'Title':titles})
-    return pd.DataFrame({'Date':dates, 'Title':titles})
+    # return pd.DataFrame({'Date':dates, 'Title':titles})
+    return pd.DataFrame({'Date':dates, 'Subj':subj, 'Title':titles})
 
 # put together different scrapers
 def etl():
-    pd = tsa_scraper()
-    write_to_file(pd)
+    URL1 = "https://www.tsa-algerie.com/politique/"
+    subj1 = 'Pol'
+    URL2 = "https://www.tsa-algerie.com/economie/"
+    subj2 = 'Eco'
+    p_politics = tsa_scraper(URL1, subj1)
+    p_econimics = tsa_scraper(URL2, subj2)
+    write_to_file(p_politics, p_econimics)
 
 etl()
